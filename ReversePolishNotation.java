f743051f0fa543ce96f4e79aa156b6c5ccb4086a
vanimittal1304 <vani9045060450@gmail.com>
2023-01-03 16:11:14 +0530

    array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.

---
+++ ReversePolishNotation.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+// Q- You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// Normal : 2+3=5
+// RPN : 23+=5
+// normal : 12-2+5=15
+// RPN : 122-5+=15
+
+// we will be using stack data structure
+
+// st[]
+// for t in tokens:
+// if t in "+-*/":
+// opd2=st.pop()
+// opd1=st.pop()
+// st.append(eval(opd1, opd2, t))
+// else
+// st.append(int(t))
+
+class maximumLengthofRotationFunction {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack();
+        for (String t : tokens) {
+            if ("+-*/".contains(t))
+                st.push(eval(st.pop(), st.pop(), t));
+            else
+                st.push(Integer.parseInt(t));
+        }
+        return st.pop();
+    }
+
+    private int eval(int b, int a, String op) {
+        if ("+".equals(op))
+            return a + b;
+        else if ("-".equals(op))
+            return a - b;
+        else if ("*".equals(op))
+            return a * b;
+        else
+            return a / b;
+
+    }
+}
