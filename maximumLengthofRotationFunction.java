c06e74f8c5318623c640fd74b74a079c35777f5b
vanimittal1304 <vani9045060450@gmail.com>
2023-01-03 16:12:01 +0530

    You are given an integer array nums of length n. Return maximum length of Rotation Function.

---
+++ maximumLengthofRotationFunction.java
@@ -0,0 +1,21 @@
+// You are given an integer array nums of length n. Return maximum length of Rotation Function.
+
+class maximumLengthofRotationFunction {
+    public int maxRotateFunction(int[] nums) {
+        final int sum = Arrays.stream(nums).sum();
+        int f = 0;
+
+        // catculate F(0) first
+        for (int i = 0; i < nums.length; ++i)
+            f += i * nums[i];
+
+        int ans = f;
+
+        for (int i = nums.length - 1; i >= 0; --i) {
+            f += sum - nums.length * nums[i];
+            ans = Math.max(ans, f);
+        }
+        return ans;
+    }
+
+}
